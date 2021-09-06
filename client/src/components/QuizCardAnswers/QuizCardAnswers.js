e56e53b06eef05fcb5c21688459815b945021b63
Ana Olekas <86985160+ShianneOlekas@users.noreply.github.com>
2021-09-05 21:59:48 -0400

    added id/value to answers

--- client/src/components/QuizCardAnswers/QuizCardAnswers.js
+++ client/src/components/QuizCardAnswers/QuizCardAnswers.js
@@ -2,16 +2,16 @@ function QuizCardAnswers({answers}) {
 
     const answersRendered = answers?.map((answer) => (
         <label>
-            <input type='radio' name='answer'/> {answer.answer}
+            <input type='radio' name={answer.id}/> {answer.answer}
         </label>
     ))
 
     console.log(answers);
       
     return (
-        <button>
+        <>
             {answersRendered}
-        </button>
+        </>
     )
 }
 
