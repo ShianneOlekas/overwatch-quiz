2ba381447d919b798571f171a1c9db3d7fcfa7bf
Alyssa <alysurr@Alyssas-MacBook-Pro.local>
2021-09-07 05:45:15 -0400

    added and styled result page, added routes

--- client/src/Pages/Result/Result.js
+++ client/src/Pages/Result/Result.js
@@ -1,9 +1,36 @@
-function Result() {
-    return (
-      <div className="result">
-          <p>Result</p>
-      </div>
-    );
-  }
-  
-  export default Result;
+import axios from 'axios';
+import { Component } from 'react';
+import ResultCard from '../../components/ResultCard/ResultCard';
+
+class Result extends Component {
+    state = {
+        results: [],
+    }
+
+  componentDidMount() {
+        axios
+            .get('http://localhost:8080/result')
+            .then((response) => {
+                console.log(response.data);
+                if(response.status === 200) {
+                    return this.setState({
+                        results: response.data,
+                    })
+                }
+            })
+      }
+        
+    render() {
+
+        return (
+            <div>
+            <ResultCard results={this.state.results}/>
+            </div>
+        );
+    }
+};
+
+export default Result;
+
+
+
